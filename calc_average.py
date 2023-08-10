20137ad5f4854a69397767acc51cef51a7858465
Ehsan Movahedian <m.ehsan.movahedian@gmail.com>
2023-08-10 10:18:25 +0330

    006: Remove points from average.

--- calc_average.py
+++ calc_average.py
@@ -17,22 +17,22 @@ try:
     data = worksheet.get_all_records()
     df = pd.DataFrame(data)
     last_rows = df.tail(count)
-    new_data = [{'symbol': symbol_name, 'data': date, 'hour': hour, 'NAV price': last_rows['NAV price'].mean(),
-                 'last trade price': last_rows['last trade price'].mean(),
-                 'deals count': last_rows['deals count'].mean(),
-                 'deals volume': last_rows['deals volume'].mean(), 'final price': last_rows['final price'].mean(),
-                 'total buyers count': last_rows['total buyers count'].mean(),
-                 'total buyers volume': last_rows['total buyers volume'].mean(),
-                 'total sellers count': last_rows['total sellers count'].mean(),
-                 'total sellers volume': last_rows['total sellers volume'].mean(),
-                 'individual buy volume': last_rows['individual buy volume'].mean(),
-                 'individual buy count': last_rows['individual buy count'].mean(),
-                 'individual sell volume': last_rows['individual sell volume'].mean(),
-                 'individual sell count': last_rows['individual sell count'].mean(),
-                 'corporate buy volume': last_rows['corporate buy volume'].mean(),
-                 'corporate buy count': last_rows['corporate buy count'].mean(),
-                 'corporate sell volume': last_rows['corporate sell volume'].mean(),
-                 'corporate sell count': last_rows['corporate sell count'].mean(), }]
+    new_data = [{'symbol': symbol_name, 'data': date, 'hour': hour, 'NAV price': round(last_rows['NAV price'].mean()),
+                 'last trade price': round(last_rows['last trade price'].mean()),
+                 'deals count': round(last_rows['deals count'].mean()),
+                 'deals volume': round(last_rows['deals volume'].mean()), 'final price': round(last_rows['final price'].mean()),
+                 'total buyers count': round(last_rows['total buyers count'].mean()),
+                 'total buyers volume': round(last_rows['total buyers volume'].mean()),
+                 'total sellers count': round(last_rows['total sellers count'].mean()),
+                 'total sellers volume': round(last_rows['total sellers volume'].mean()),
+                 'individual buy volume': round(last_rows['individual buy volume'].mean()),
+                 'individual buy count': round(last_rows['individual buy count'].mean()),
+                 'individual sell volume': round(last_rows['individual sell volume'].mean()),
+                 'individual sell count': round(last_rows['individual sell count'].mean()),
+                 'corporate buy volume': round(last_rows['corporate buy volume'].mean()),
+                 'corporate buy count': round(last_rows['corporate buy count'].mean()),
+                 'corporate sell volume': round(last_rows['corporate sell volume'].mean()),
+                 'corporate sell count': round(last_rows['corporate sell count'].mean()), }]
     df = pd.DataFrame(new_data)
     sheet_name = f'{symbol_name}_{count}'
     sh1 = gc.open(sheet_name)
