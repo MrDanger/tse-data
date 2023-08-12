c8489c9b246a43f76158adc1f0de265d0931fa01
Ehsan Movahedian <m.ehsan.movahedian@gmail.com>
2023-08-12 11:41:00 +0330

    007: Read from sheet.

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from scheduler import run_continuous_lazy, run_lazy
 from dotenv import load_dotenv
 import os
-from datetime import datetime, timedelta
+from datetime import datetime
+import gspread
 
 load_dotenv()
-symbols = os.environ.get('STOCK_NAMES').split(',')
+sheet_name = os.environ.get('SHEET_NAME', default='temp')
+credential = 'credentials.json'
+gc = gspread.service_account(filename=credential)
+sh = gc.open(sheet_name)
+
+sheet_names = sh.worksheets()
+
+symbols = list(set([sheet.title[:-2].replace(' ', '_') for sheet in sheet_names]))
+print(f'activating  for  symbols: {symbols}')
+
 start_hour = int(os.environ.get('START_HOUR', default=9))
 end_hour = int(os.environ.get('END_HOUR', default=15))
 now = datetime.now()
@@ -16,10 +26,10 @@ try:
         run_continuous_lazy(f'./fetch_data.py {symbol}', 'h', 60, end_time.strftime('%Y-%m-%dT%H:%M:%S'),
                             start_time.strftime('%Y-%m-%dT%H:%M:%S'))
         run_lazy(f'./fetch_data.py {symbol}', 0, end_hour - 1, 59, 30)
-        run_continuous_lazy(f'./calc_average.py ' + symbol + ' 3', 'h', 180,
+        run_continuous_lazy(f'./calc_average.py 3 ' + symbol, 'h', 180,
                             now.replace(hour=end_hour, minute=0, second=1).strftime('%Y-%m-%dT%H:%M:%S'),
                             now.replace(hour=start_hour, minute=3, second=1).strftime('%Y-%m-%dT%H:%M:%S'))
-        run_continuous_lazy(f'./calc_average.py {symbol} 5', 'h', 300,
+        run_continuous_lazy(f'./calc_average.py 5 {symbol}', 'h', 300,
                             now.replace(hour=end_hour, minute=0, second=1).strftime('%Y-%m-%dT%H:%M:%S'),
                             now.replace(hour=start_hour, minute=5, second=1).strftime('%Y-%m-%dT%H:%M:%S'))
 
